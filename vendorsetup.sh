c2d701b3da2234f760ab8a437a691d2b89a1cd1b
beanstown106 <nbenis106@gmail.com>
2015-05-04 23:34:02 -0400

    switch to user builds

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo nexus_grouper-userdebug
-add_lunch_combo nexus_flo-userdebug
-add_lunch_combo nexus_shamu-userdebug
-add_lunch_combo nexus_flounder-userdebug
+add_lunch_combo nexus_grouper-user
+add_lunch_combo nexus_flo-user
+add_lunch_combo nexus_shamu-user
+add_lunch_combo nexus_flounder-user
