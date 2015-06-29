b9f3f288b1f2a32b3b9270669685c637dfe79c24
beanstown106 <nbenis106@gmail.com>
2015-06-28 22:16:35 -0400

    Add GoogleCamera to prebuilt

--- configs/prebuilt.mk
+++ configs/prebuilt.mk
@@ -22,11 +22,35 @@ PRODUCT_PACKAGES += \
     BrowserProviderProxy \
     CalendarGooglePrebuilt \
     Chrome \
+    GoogleCamera \
     LatinImeGoogle
 
 ifneq ($(filter nexus_hammerhead nexus_shamu,$(TARGET_PRODUCT)),)
 PRODUCT_PACKAGES += \
     PrebuiltBugle
+endif
+
+# Google Camera dependancies
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/lib/libgcam.so:system/app/GoogleCamera/lib/arm/libgcam.so \
+    vendor/nexus/prebuilt/lib/libgcam_swig_jni.so:system/app/GoogleCamera/lib/arm/libgcam_swig_jni.so \
+    vendor/nexus/prebuilt/lib/libjni_jpegutil.so:system/app/GoogleCamera/lib/arm/libjni_jpegutil.so \
+    vendor/nexus/prebuilt/lib/libjni_tinyplanet.so:system/app/GoogleCamera/lib/arm/libjni_tinyplanet.so \
+    vendor/nexus/prebuilt/lib/libjpeg.so:system/app/GoogleCamera/lib/arm/libjpeg.so \
+    vendor/nexus/prebuilt/lib/liblightcycle.so:system/app/GoogleCamera/lib/arm/liblightcycle.so \
+    vendor/nexus/prebuilt/lib/libnativehelper_compat_libc++.so:system/app/GoogleCamera/lib/arm/libnativehelper_compat_libc++.so \
+    vendor/nexus/prebuilt/lib/librefocus.so:system/app/GoogleCamera/lib/arm/librefocus.so \
+    vendor/nexus/prebuilt/lib/librsjni.so:system/app/GoogleCamera/lib/arm/librsjni.so \
+    vendor/nexus/prebuilt/lib/librs.layered_filter_f32.so:system/app/GoogleCamera/lib/arm/librs.layered_filter_f32.so \
+    vendor/nexus/prebuilt/lib/librs.layered_filter_fast_f32.so:system/app/GoogleCamera/lib/arm/librs.layered_filter_fast_f32.so \
+    vendor/nexus/prebuilt/lib/libRSSupport.so:system/app/GoogleCamera/lib/arm/libRSSupport.so
+ifneq ($(filter nexus_hammerhead nexus_shamu,$(TARGET_PRODUCT)),)
+PRODUCT_PACKAGES += \
+    com.google.android.camera2
+endif
+ifneq ($(filter nexus_hammerhead nexus_shamu,$(TARGET_PRODUCT)),)
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/etc/permissions/com.google.android.camera2.xml:system/etc/permissions/com.google.android.camera2.xml
 endif
 
 # Google Chrome dependancies
