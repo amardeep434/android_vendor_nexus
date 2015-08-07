417326b6b27f0a51a36db845a4ba2c57624042ef
beanstown106 <nbenis106@gmail.com>
2015-08-07 15:54:51 -0400

    Add apns for tablets

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -26,6 +26,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# Google Apns for tablets
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/etc/apns.conf:system/etc/apns.conf
+
 # Proprietary latinime lib needed for Keyboard swyping
 PRODUCT_COPY_FILES += \
     vendor/nexus/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so
