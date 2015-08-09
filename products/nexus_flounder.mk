439b341ab6b4d7b06de84a395efe86a328ae07b7
beanstown106 <nbenis106@gmail.com>
2015-08-09 19:32:25 -0400

    update flunder build/fingerprint

--- products/nexus_flounder.mk
+++ products/nexus_flounder.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := htc
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=volantis \
-    BUILD_FINGERPRINT=google/volantis/flounder:5.1.1/LMY47X/1849464:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 5.1.1 LMY47X 1849464 release-keys"
+    BUILD_FINGERPRINT=google/volantis/flounder:5.1.1/LMY48I/2074855:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 5.1.1 LMY48I 2074855 release-keys"
